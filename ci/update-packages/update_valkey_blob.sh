bf978a9f16346bff73d46dbf483098433594b914
Philipp Thun <philipp.thun@sap.com>
2024-06-19 11:36:49 +0200

    Update bump-valkey-blob (#63)

--- ci/update-packages/update_valkey_blob.sh
+++ ci/update-packages/update_valkey_blob.sh
@@ -19,14 +19,14 @@ echo "${PRIVATE_YAML}" > "$PWD"/capi-release/config/private.yml
 
 capi_blobs_path="$PWD/capi-release/config/blobs.yml"
 
-#current_valkey_blob_name=$(grep -m 1  "valkey" "$capi_blobs_path" | awk -F':' '{print $1}') || { echo "Error: grep command failed."; exit 1; }
-#current_valkey_version=$(echo "${current_valkey_blob_name}" | awk -F'/' '{print $2}' | awk -F'.tar.gz' '{print $1}') || { echo "Error: awk command failed."; exit 1; }
-#echo "Current Valkey version is '${current_valkey_version}'"
+current_valkey_blob_name=$(grep -m 1  "valkey" "$capi_blobs_path" | awk -F':' '{print $1}') || { echo "Error: grep command failed."; exit 1; }
+current_valkey_version=$(echo "${current_valkey_blob_name}" | awk -F'/' '{print $2}' | awk -F'.tar.gz' '{print $1}') || { echo "Error: awk command failed."; exit 1; }
+echo "Current Valkey version is '${current_valkey_version}'"
 
-#if [ -z "$current_valkey_blob_name" ] || [ -z "$current_valkey_version" ]; then
-#  echo "Either no Valkey entry found or no version found for Valkey in blobs.yml."
-#  exit 1
-#fi
+if [ -z "$current_valkey_blob_name" ] || [ -z "$current_valkey_version" ]; then
+  echo "Either no Valkey entry found or no version found for Valkey in blobs.yml."
+  exit 1
+fi
 
 valkey_path="$PWD/valkey-release"
 new_valkey_version=$(cat "$valkey_path/version") || { echo "Error: cat command for version failed."; exit 1; }
@@ -35,18 +35,18 @@ echo "New Valkey version is '${new_valkey_version}'"
 
 cp -r "$PWD"/capi-release/. "$PWD"/updated-capi-release
 
-#if [[ "$current_valkey_version" == "$new_valkey_version" ]]; then
-#  echo "The current Valkey version is the same as the new version. Exiting..."
-#  exit 0
-#fi
+if [[ "$current_valkey_version" == "$new_valkey_version" ]]; then
+  echo "The current Valkey version is the same as the new version. Exiting..."
+  exit 0
+fi
 
 pushd capi-release
-    #bosh remove-blob -n "${current_valkey_blob_name}"
+    bosh remove-blob -n "${current_valkey_blob_name}"
     bosh add-blob -n "$valkey_path/source.tar.gz" valkey/"${new_valkey_version}".tar.gz
 
-    #sed -i "0,/$current_valkey_version/s//$new_valkey_version/" packages/valkey/packaging || { echo "Error: sed command for 'packaging' failed."; exit 1; }
-    #sed -i "s/$current_valkey_version/$new_valkey_version/g" packages/valkey/README.md || { echo "Error: sed command for 'README' failed."; exit 1; }
-    #sed -i "0,/$current_valkey_version/s//$new_valkey_version/" packages/valkey/spec || { echo "Error: sed command for 'spec' failed."; exit 1; }
+    sed -i "0,/$current_valkey_version/s//$new_valkey_version/" packages/valkey/packaging || { echo "Error: sed command for 'packaging' failed."; exit 1; }
+    sed -i "s/$current_valkey_version/$new_valkey_version/g" packages/valkey/README.md || { echo "Error: sed command for 'README' failed."; exit 1; }
+    sed -i "0,/$current_valkey_version/s//$new_valkey_version/" packages/valkey/spec || { echo "Error: sed command for 'spec' failed."; exit 1; }
 
     bosh upload-blobs -n
 
