52961e21809c7d3f926f235748464be9c112c99c
Philipp Thun <philipp.thun@sap.com>
2024-08-08 14:12:12 +0200

    Helper script to set all pipelines (#100)

---
+++ set_all_pipelines.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <target>"
+  exit 1
+fi
+
+target=$1
+
+# List of pipeline files
+files=(
+  "./ci/pipeline-bosh-lite.yml"
+  "./ci/pipeline-certificate-validation.yml"
+  "./ci/cve-pipeline.yml"
+  "./ci/pipeline.yml"
+  "./ci/pipeline-docker.yml"
+)
+
+# Corresponding pipeline names
+names=(
+  "bosh-lites"
+  "capi-cert-validation"
+  "cve-scan"
+  "capi"
+  "docker-builds"
+)
+
+if [ ${#files[@]} -ne ${#names[@]} ]; then
+  echo "Error: The number of files does not match the number of pipeline names."
+  exit 1
+fi
+
+# Iterate over the arrays and call the 'fly set-pipeline' command
+for i in "${!files[@]}"; do
+  file="${files[$i]}"
+  name="${names[$i]}"
+  echo "Setting pipeline '$name' from file '$file'"
+  fly -t "$target" set-pipeline -p "$name" -c "$file"
+done
