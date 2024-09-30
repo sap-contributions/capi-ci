5736743de64aaa0cae12420f4810ca5d804852e7
Philipp Thun <philipp.thun@sap.com>
2024-09-30 12:02:34 +0200

    Use new key=value format and remove bump-bbl-version job (#102)

--- ci/dockerfiles/bump_bosh_cli_version.sh
+++ ci/dockerfiles/bump_bosh_cli_version.sh
@@ -6,7 +6,7 @@ BOSH_CLI_VERSION=$(cat bosh-cli-github-release/version)
 
 pushd capi-dockerfiles > /dev/null
   for dockerfile in $(grep -Rl "ENV bosh_cli_version" .); do
-    sed -i "s/ENV bosh_cli_version.*$/ENV bosh_cli_version $BOSH_CLI_VERSION/" $dockerfile
+    sed -i "s/ENV bosh_cli_version.*$/ENV bosh_cli_version=$BOSH_CLI_VERSION/" $dockerfile
   done
 
   if [[ -n $(git status --porcelain) ]]; then
