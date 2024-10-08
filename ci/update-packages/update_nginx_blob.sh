615bcecdac7adbe77d9fc3ac19af7bedfb466dbc
Jochen Ehret <jochen.ehret@sap.com>
2024-10-08 10:45:15 +0200

    Fix nginx update task (#105)
    * replace versions in "nginx_webdav" package
    * exponential retry for wget download (archive may not yet be available on nginx.org when github tag is published)

--- ci/update-packages/update_nginx_blob.sh
+++ ci/update-packages/update_nginx_blob.sh
@@ -34,8 +34,27 @@ else
     echo "Importing PGP key ${pgp_key}"
     gpg --import "${pgp_key}"
   done
-  echo "Downloading nginx archive..."
-  wget -O "nginx-${new_nginx_version}.tar.gz"     "https://nginx.org/download/nginx-${new_nginx_version}.tar.gz"
+
+  # archive may not yet be ready immediately for download on nginx.org -> exponential retry
+  attempt=1
+  max_attempts=4
+  retry_delay_sec=300
+  while (( attempt <= max_attempts )); do
+    echo "Downloading nginx archive (attempt $attempt/$max_attempts)..."
+    if ! wget -O "nginx-${new_nginx_version}.tar.gz" "https://nginx.org/download/nginx-${new_nginx_version}.tar.gz"; then
+      echo "Command failed, retrying in $retry_delay_sec seconds"
+      sleep $retry_delay_sec
+      retry_delay_sec=$((retry_delay_sec * 2)) # double the retry delay for the next attempt
+      if (( attempt == max_attempts )); then
+        echo "Failed to download new nginx archive after $max_attempts retries. Retrigger job when https://nginx.org/download/nginx-${new_nginx_version}.tar.gz is available."
+        exit 1
+      fi
+      ((attempt++))
+    else
+      break
+    fi
+  done
+
   echo "Downloading nginx archive signature file..."
   wget -O "nginx-${new_nginx_version}.tar.gz.asc" "https://nginx.org/download/nginx-${new_nginx_version}.tar.gz.asc"
   echo "Verifying signature for nginx archive..."
@@ -45,9 +64,12 @@ else
     bosh remove-blob -n "${current_nginx_blob_name}"
     bosh add-blob -n "../nginx-${new_nginx_version}.tar.gz" nginx/nginx-"${new_nginx_version}".tar.gz
 
-    sed -i "s/nginx-$current_nginx_version/nginx-$new_nginx_version/g" packages/nginx/packaging || { echo "Error: sed command for 'packaging' failed."; exit 1; }
-    sed -i "s/nginx-$current_nginx_version/nginx-$new_nginx_version/g" packages/nginx/README.md || { echo "Error: sed command for 'README' failed."; exit 1; }
-    sed -i "s/nginx-$current_nginx_version/nginx-$new_nginx_version/g" packages/nginx/spec || { echo "Error: sed command for 'spec' failed."; exit 1; }
+    sed -i "s/nginx-$current_nginx_version/nginx-$new_nginx_version/g" packages/nginx/packaging || { echo "Error: sed command for 'packages/nginx/packaging' failed."; exit 1; }
+    sed -i "s/nginx-$current_nginx_version/nginx-$new_nginx_version/g" packages/nginx/README.md || { echo "Error: sed command for 'packages/nginx/README.md' failed."; exit 1; }
+    sed -i "s/nginx-$current_nginx_version/nginx-$new_nginx_version/g" packages/nginx/spec || { echo "Error: sed command for 'packages/nginx/spec' failed."; exit 1; }
+
+    sed -i "s/nginx-$current_nginx_version/nginx-$new_nginx_version/g" packages/nginx_webdav/packaging || { echo "Error: sed command for 'packages/nginx_webdav/packaging' failed."; exit 1; }
+    sed -i "s/nginx-$current_nginx_version/nginx-$new_nginx_version/g" packages/nginx_webdav/spec || { echo "Error: sed command for 'packages/nginx_webdav/spec' failed."; exit 1; }
 
     bosh upload-blobs -n
 
