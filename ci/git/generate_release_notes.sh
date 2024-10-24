379623b6f90925271e65f1140b2a9d9cb2172bf1
Jochen Ehret <jochen.ehret@sap.com>
2024-10-24 13:27:18 +0200

    Move release notes before "ship-it" jobs
    * print the diff between the last "ci-passed" revision and the last released revision from "main"
    * move job after "bump-ci-passed"

--- ci/git/generate_release_notes.sh
+++ ci/git/generate_release_notes.sh
@@ -4,10 +4,14 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-pushd capi-release > /dev/null
-  mapfile -t VERSIONS < <(git tag --sort=-v:refname | grep '^1\.' | head -n2)
-  VERSION=${VERSIONS[0]}
-  PREVIOUS_VERSION=${VERSIONS[1]}
-
-  "$SCRIPT_DIR/release_notes.rb" $PREVIOUS_VERSION $VERSION
+pushd capi-release-ci-passed > /dev/null
+  # get latest commit id of "ci-passed" branch (= new release candidate)
+  CI_PASSED_VERSION="$(git rev-parse HEAD)"
 popd > /dev/null
+
+pushd capi-release-main > /dev/null
+  # get tag of last release from "main" branch
+  PREVIOUS_VERSION="$(git describe --tags --abbrev=0)"
+popd > /dev/null
+
+"$SCRIPT_DIR/release_notes.rb" "$PREVIOUS_VERSION" "$CI_PASSED_VERSION"
