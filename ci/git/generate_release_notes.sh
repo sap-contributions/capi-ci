f729dce74a0fbb09f3f429ea661259fd3335d600
Jochen Ehret <jochen.ehret@sap.com>
2024-10-25 13:19:59 +0200

    Fix script call (must be inside git repo)

--- ci/git/generate_release_notes.sh
+++ ci/git/generate_release_notes.sh
@@ -4,14 +4,13 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-pushd capi-release-ci-passed > /dev/null
-  # get latest commit id of "ci-passed" branch (= new release candidate)
-  CI_PASSED_VERSION="$(git rev-parse HEAD)"
-popd > /dev/null
-
 pushd capi-release-main > /dev/null
   # get tag of last release from "main" branch
   PREVIOUS_VERSION="$(git describe --tags --abbrev=0)"
 popd > /dev/null
 
-"$SCRIPT_DIR/release_notes.rb" "$PREVIOUS_VERSION" "$CI_PASSED_VERSION"
+pushd capi-release-ci-passed > /dev/null
+  # get latest commit id of "ci-passed" branch (= new release candidate)
+  CI_PASSED_VERSION="$(git rev-parse HEAD)"
+  "$SCRIPT_DIR/release_notes.rb" "$PREVIOUS_VERSION" "$CI_PASSED_VERSION"
+popd > /dev/null
